c5086479cf74fd12885f97bef376cd01ebb633fb
Katia Gibson <kcygibson@gmail.com>
2021-07-13 09:01:32 -0400

    adding actual images to assets folder to test, working on img load bug, cards are appending correctly otherwise

--- data/hero-db.js
+++ data/hero-db.js
@@ -3,12 +3,12 @@ export {
 }
 
 const heroes = [
-  {src: `https://static.wikia.nocookie.net/marvelcinematicuniverse/images/3/35/IronMan-EndgameProfile.jpg/revision/latest?cb=20190423175213` , name: 'Ironman', alias: "Tony Stark", status: "Dead", _id: 641198},
-  {src: `https://static.wikia.nocookie.net/marvelcinematicuniverse/images/7/77/WarMachine-EndgameProfile.jpg/revision/latest?cb=20190423174940`, name: 'War Machine', alias: "Colonel James 'Rhodey' Rhoades", status: "Alive", _id: 512236},
-  {src: `https://static.wikia.nocookie.net/marvelcinematicuniverse/images/9/9a/BlackWidow-EndgameProfile.jpg/revision/latest?cb=20190423174842`, name: 'Black Widow', alias: "Natasha Romanoff", status: "Dead", _id: 304167},
-  {src: `https://static.wikia.nocookie.net/marvelcinematicuniverse/images/a/a4/ProfHulkAvengersEndgamelicensingart001.jpg/revision/latest?cb=20190506122658`, name: 'Hulk', alias: "Bruce Banner", status: "Alive", _id: 266334},
-  {src: `https://static.wikia.nocookie.net/marvelcinematicuniverse/images/1/13/Thor-EndgameProfile.jpg/revision/latest?cb=20190423174911`, name: 'Thor Odinson', alias: "God of Thunder", status: "Alive", _id: 655055},
-  {src: `https://static.wikia.nocookie.net/marvelcinematicuniverse/images/0/08/HawkeyeRonin-EndgameProfile.jpg/revision/latest?cb=20190423175147`, name: 'Hawkeye', alias: "Clint Barton", status: "Alive", _id: 551843},
+  {src: `/images/IronMan-EndgameProfile.jpeg` , name: 'Ironman', alias: "Tony Stark", status: "Dead", _id: 641198},
+  {src: `/images/WarMachine-EndgameProfile.jpeg`, name: 'War Machine', alias: "Colonel James 'Rhodey' Rhoades", status: "Alive", _id: 512236},
+  {src: `/images/BlackWidow-EndgameProfile.jpeg`, name: 'Black Widow', alias: "Natasha Romanoff", status: "Dead", _id: 304167},
+  {src: `/images/ProfHulkAvengersEndgamelicensingart001.jpeg`, name: 'Hulk', alias: "Bruce Banner", status: "Alive", _id: 266334},
+  {src: `/images/Thor-EndgameProfile.jpeg`, name: 'Thor Odinson', alias: "God of Thunder", status: "Alive", _id: 655055},
+  {src: `/images/HawkeyeRonin-EndgameProfile.jpeg`, name: 'Hawkeye', alias: "Clint Barton", status: "Alive", _id: 551843},
 ]
 
 const find = (conditions, callback) => {
